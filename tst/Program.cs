bee2f30880466342c5a71150e8fecb53449a6ef7
Igor Pr <igor.prokofjev@tktk.ee>
2023-04-11 00:12:08 +0300

    add Week8EpicGame code

---
+++ tst/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+string path = @"C:\Users\Igor\Desktop\Data2";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+ShowFileContent();
+
+static int ShowFileContent(string[] fileContentArray) 
+{
+    int i = 0;
+    foreach(CallerLineNumberAttribute in fileContentArray);
+}
